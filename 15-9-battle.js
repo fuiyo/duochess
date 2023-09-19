e8a3d23e63bdbea3311baee2ecae87ce3b2cc7b2
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2023-09-19 22:19:12 +0800

    Update 15-9-battle.js

--- 15-9-battle.js
+++ 15-9-battle.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const fs = require('fs');
 
 (async () => {
-const participants = await axios(`https://api.challonge.com/v1/tournaments/13468288/participants.json?api_key=V7xd1G8VoUp31zfmqvmCrnemBtXjDVmI6rc8fHn8`)
+const participants = await axios(`https://api.challonge.com/v1/tournaments/13486446/participants.json?api_key=V7xd1G8VoUp31zfmqvmCrnemBtXjDVmI6rc8fHn8`)
 const data = [];
-const groups = await axios(`https://api.challonge.com/v1/tournaments/13468288/matches.json?api_key=V7xd1G8VoUp31zfmqvmCrnemBtXjDVmI6rc8fHn8`)
+const groups = await axios(`https://api.challonge.com/v1/tournaments/13486446/matches.json?api_key=V7xd1G8VoUp31zfmqvmCrnemBtXjDVmI6rc8fHn8`)
 groups.data.forEach((e, index) => {
 const player1 = e.match.player1_id;
 const player2 = e.match.player2_id;
@@ -15,5 +15,5 @@ const participant2 = participants.data.find(e=>e?.participant?.id == player2)
 data.push([participant1.participant.name, participant2.participant.name])
 })
 
-fs.writeFileSync("./Day 2.json", JSON.stringify(data))
+fs.writeFileSync("./Day 3.json", JSON.stringify(data))
 })();
